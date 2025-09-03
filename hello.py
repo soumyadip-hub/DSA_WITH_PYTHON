3fa820d9ddf6ad5d1132825aa45a0a7b11953438
noobtolearner <soumyadippakhira094@gmail.com>
2025-09-03 06:53:53 +0530

    acess array element

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
 print("hello DSa")
+print("hello")
+print("big o is better and efficientlly")
+print(
+    "in time complexity we need to look at amount of time taken by an algorithm to run as a function of the length"
+)
+print(
+    "in time of space complexity  in defination using computer requires memory to hold temporary data"
+)
+print("going to see about Big O notation ")
+print(
+    "in case1 Big O  it is a complexity that is going to be less or equal to the worst case"
+)
+print(
+    "big omega    it is a complexity that is within bounds of the worst and the best cases"
+)
