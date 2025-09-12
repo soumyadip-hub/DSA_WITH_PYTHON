18580cdcd445b81d96860ebb70b153b3444854e0
noobtolearner <soumyadippakhira094@gmail.com>
2025-09-12 08:34:00 +0530

    slice/delete list -----> then learn searching for an element--------> then linear search---------->list operation and function

--- list_operations.py
+++ list_operations.py
@@ -23,3 +23,53 @@ mylist.insert(4, 11)
 mylist.append(55)
 mylist.extend(newlist)
 print(mylist)
+
+# slice/delete  list
+
+mylist = ["a", "b", "c", "d", "e", "f"]
+
+print(mylist[0:2])
+print(mylist[1:])
+print(mylist.pop(2))
+print(mylist)
+del mylist[1]
+print(mylist)
+if "c" in mylist:
+    mylist.remove("c")
+print(mylist)
+
+# searching for an element in list
+
+mylist = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+
+target = 50
+if target in mylist:
+    print(f"{target} is in list")
+else:
+    print(f"{target} is not in the list")
+
+
+# -----------linear search--------
+def linear_search(p_list, p_target):
+    for i, value in enumerate(p_list):
+        if value == p_target:
+            return i
+    return -1
+
+
+linear_search(mylist, target)
+
+# -------list operation/functions---------
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = a + b
+print(c)
+a = [0, 1]
+a = a * 4
+print(a)
+d = [0, 12, 3, 34, 432, 3222, 940, 888]
+print(min(d))
+print(max(d))
+print(len(d))
+print(sum(a))
+print(sum(d) / len(d))
