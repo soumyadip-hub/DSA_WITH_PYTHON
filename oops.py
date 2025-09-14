39e44cd5fcd22c178fbb3f2f9c99122ee10ad8e7
noobtolearner <soumyadippakhira094@gmail.com>
2025-09-14 17:53:35 +0530

    example2

--- oops.py
+++ oops.py
@@ -1,16 +1,16 @@
-class StarCookie:
-    def __init__(self, color, weight):
-        self.color = color
-        self.weight = weight
-        pass
+# class StarCookie:
+#     def __init__(self, color, weight):
+#         self.color = color
+#         self.weight = weight
+#         pass
 
 
-star_cookie1 = StarCookie("Red", 16)
-print(star_cookie1.color)
-print(star_cookie1.weight)
-star_cookie2 = StarCookie("green", 20)
-print(star_cookie2.weight)
-print(star_cookie2.color)
+# star_cookie1 = StarCookie("Red", 16)
+# print(star_cookie1.color)
+# print(star_cookie1.weight)
+# star_cookie2 = StarCookie("green", 20)
+# print(star_cookie2.weight)
+# print(star_cookie2.color)
 
 # insted of doing one by one objects intialized(it is time wasting) ..... we use intializer or constructor to do in easy way
 
@@ -22,3 +22,21 @@ print(star_cookie2.color)
 #          class StarCookie:
 #               def __init__(self,color):
 #                       self.color = color
+
+# example 2
+
+
+class youtube:
+    def __init__(self, username, subscribers=0):
+        self.username = username
+        self.subscribers = subscribers
+
+
+user1 = youtube("abhishek")
+print(user1.username)
+print(user1.subscribers)
+
+user2 = youtube("rahul")
+user2.subscribers = 5
+print(user2.username)
+print(user2.subscribers)
