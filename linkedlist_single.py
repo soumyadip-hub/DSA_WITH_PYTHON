c7b12a9a4210a5da6150936fb539b8f57a186046
noobtolearner <soumyadippakhira094@gmail.com>
2025-09-15 06:17:20 +0530

    done

---
+++ linkedlist_single.py
@@ -0,0 +1,2 @@
+# What is LinkedList -: Linked list is a form of a sequential collection and it does not have to be in order.A Linkedlist is made up of independent nodes that may contain any type of data and each nodes has a reference to the next node in the link
+# TYPES OF LINKED LIST -- - - (i)singly linked list (ii)circular singly linked list (iii)Doubly linked list (iv)circular Doubly linked list
--- oops_class_attribute.py
+++ oops_class_attribute.py
@@ -1,16 +1,16 @@
-# class StarCookie:
-#     def __init__(self, color, weight):
-#         self.color = color
-#         self.weight = weight
-#         pass
+class StarCookie:
+    def __init__(self, color, weight):
+        self.color = color
+        self.weight = weight
+        pass
 
 
-# star_cookie1 = StarCookie("Red", 16)
-# print(star_cookie1.color)
-# print(star_cookie1.weight)
-# star_cookie2 = StarCookie("green", 20)
-# print(star_cookie2.weight)
-# print(star_cookie2.color)
+star_cookie1 = StarCookie("Red", 16)
+print(star_cookie1.color)
+print(star_cookie1.weight)
+star_cookie2 = StarCookie("green", 20)
+print(star_cookie2.weight)
+print(star_cookie2.color)
 
 # insted of doing one by one objects intialized(it is time wasting) ..... we use intializer or constructor to do in easy way
 
