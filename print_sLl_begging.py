19fbf4a8847b6b4128b455214dd0bbb0b4738b55
noobtolearner <soumyadippakhira094@gmail.com>
2025-09-16 21:40:10 +0530

    add node in the beginning of sll

---
+++ print_sLl_begging.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def __str__(self):
+        temp_node = self.head
+        result = ""
+        while temp_node is not None:
+            result += str(temp_node.value)
+            if temp_node.next is not None:
+                result += " --> "
+            temp_node = temp_node.next
+        return result
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:  # if the list is empty
+            self.head = new_node
+            self.tail = new_node
+        else:  # otherwise, attach new_node at the end
+            self.tail.next = new_node  # type: ignore
+            self.tail = new_node
+        self.length += 1
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head  # type: ignore
+            self.head = new_node
+        self.length += 1
+
+
+# testing
+new_linked_list = LinkedList()
+new_linked_list.append(10)
+new_linked_list.append(20)
+new_linked_list.append(30)
+new_linked_list.append(40)
+new_linked_list.append(50)
+print(new_linked_list)
+new_linked_list.prepend(60)
+print(new_linked_list)
